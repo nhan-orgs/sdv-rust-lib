71465c6ad37a1e0dbba51a871dae132c91f1f5d9
NTTVy03 <trucvy05062003@gmail.com>
2024-08-01 15:09:48 +0700

    refactor: extract variables from match staments

--- src/kuksa_client.rs
+++ src/kuksa_client.rs
@@ -27,25 +27,16 @@ impl KuksaClient {
     }
 
     pub async fn connect(&mut self) -> Result<(), ClientError> {
-        match &self.client {
-            Some(_) => {
-                // already connected
-                return Ok(());
-            }
-            None => {
-                match ValClient::connect(self.server_address.clone()).await {
-                    Ok(client) => {
-                        self.client = Some(client);
-                        return Ok(());
-                    }
-                    Err(_) => {
-                        return Err(ClientError::Connection(
-                            "Can not connect ValClient".to_string(),
-                        ));
-                    }
-                };
-            }
+        if self.client.is_some() {
+            return Ok(());
         }
+
+        ValClient::connect(self.server_address.clone())
+            .await
+            .map(|client| {
+                self.client = Some(client);
+            })
+            .map_err(|_| ClientError::Connection("Can not connect ValClient".to_string()))
     }
 
     pub async fn get(
@@ -54,90 +45,85 @@ impl KuksaClient {
         view: i32,
         fields: Vec<i32>,
     ) -> Result<Vec<DataEntry>, ClientError> {
-        match self.client {
+        let client = match self.client {
             None => {
-                // TODO: connect to server
-                Err(ClientError::Connection(
+                return Err(ClientError::Connection(
                     "Please connect to server".to_string(),
-                ))
+                ));
             }
-            Some(ref mut client) => {
-                let request = GetRequest {
-                    entries: vec![EntryRequest {
-                        path: path.to_string(),
-                        view: view,
-                        fields: fields,
-                    }],
-                };
+            Some(ref mut client) => client,
+        };
 
-                match client.get(Request::new(request)).await {
-                    Ok(response) => {
-                        let message = response.into_inner();
+        let request = GetRequest {
+            entries: vec![EntryRequest {
+                path: path.to_string(),
+                view: view,
+                fields: fields,
+            }],
+        };
 
-                        // collect errors from response
-                        let mut errors = vec![];
-
-                        if let Some(err) = message.error {
-                            errors.push(err);
-                        }
-
-                        for error in message.errors {
-                            if let Some(err) = error.error {
-                                errors.push(err);
-                            }
-                        }
-
-                        // check if return error or entries' value
-                        if errors.len() > 0 {
-                            return Err(ClientError::Function(errors));
-                        } else {
-                            return Ok(message.entries);
-                        }
-                    }
-                    Err(error) => {
-                        return Err(ClientError::Status(error));
-                    }
-                }
+        let message = match client.get(Request::new(request)).await {
+            Ok(response) => response.into_inner(),
+            Err(error) => {
+                return Err(ClientError::Status(error));
             }
+        };
+
+        // collect errors from response
+        let mut errors = vec![];
+
+        if let Some(err) = message.error {
+            errors.push(err);
+        }
+
+        for error in message.errors {
+            if let Some(err) = error.error {
+                errors.push(err);
+            }
+        }
+
+        // check if return error or entries' value
+        if errors.len() > 0 {
+            return Err(ClientError::Function(errors));
+        } else {
+            return Ok(message.entries);
         }
     }
 
     pub async fn set(&mut self, entries: Vec<EntryUpdate>) -> Result<(), ClientError> {
-        match self.client {
+        let client = match self.client {
             None => {
                 return Err(ClientError::Connection(
                     "Please connect to server".to_string(),
-                ))
+                ));
             }
-            Some(ref mut client) => {
-                let request = SetRequest { updates: entries };
+            Some(ref mut client) => client,
+        };
 
-                match client.set(request).await {
-                    Ok(response) => {
-                        let message = response.into_inner();
+        let request = SetRequest { updates: entries };
 
-                        // collect errors from response
-                        let mut errors = vec![];
+        let message = match client.set(request).await {
+            Ok(response) => response.into_inner(),
+            Err(err) => return Err(ClientError::Status(err)),
+        };
 
-                        if let Some(err) = message.error {
-                            errors.push(err);
-                        }
+        // collect errors from response
+        let mut errors = vec![];
 
-                        for error in message.errors {
-                            if let Some(err) = error.error {
-                                errors.push(err);
-                            }
-                        }
+        if let Some(err) = message.error {
+            errors.push(err);
+        }
 
-                        if errors.len() > 0 {
-                            return Err(ClientError::Function(errors));
-                        } else {
-                            return Ok(());
-                        }
-                    }
-                    Err(err) => return Err(ClientError::Status(err)),
-                }
+        for error in message.errors {
+            if let Some(err) = error.error {
+                errors.push(err);
             }
+        }
+
+        if errors.len() > 0 {
+            return Err(ClientError::Function(errors));
+        } else {
+            return Ok(());
         }
     }
 
@@ -176,28 +162,26 @@ impl KuksaClient {
         &mut self,
         entry_path: &str,
     ) -> Result<HashMap<String, DataType>, ClientError> {
-        match self.get_metadata(entry_path).await {
-            Ok(metadatas) => {
-                let mut result = HashMap::new();
-
-                for metadata in metadatas {
-                    match DataType::try_from(metadata.1.data_type) {
-                        Ok(datatype) => {
-                            result.insert(metadata.0, datatype);
-                        }
-                        Err(_error) => {
-                            println!("Decode error:  DataType::try_from() failed");
-                            return Err(ClientError::Function(vec![]));
-                        }
-                    }
-                }
-
-                Ok(result)
-            }
+        let metadatas = match self.get_metadata(entry_path).await {
+            Ok(metadatas) => metadatas,
             Err(error) => {
                 return Err(error);
             }
+        };
+
+        let mut result = HashMap::new();
+
+        for metadata in metadatas {
+            let datatype = match DataType::try_from(metadata.1.data_type) {
+                Ok(datatype) => datatype,
+                Err(_error) => {
+                    return Err(ClientError::Function(vec![]));
+                }
+            };
+            result.insert(metadata.0, datatype);
         }
+
+        Ok(result)
     }
 
     pub async fn get_entry_data(&mut self, path: &str) -> Result<Option<Datapoint>, ClientError> {
@@ -271,56 +255,47 @@ impl KuksaClient {
         println!("value: {:?}", value);
         println!();
 
-        match self.get_datatype(entry_path).await {
-            Ok(datatype) => {
-                // datatype to value
-                // check if entry_path exist in datatype hashmap
-                if !datatype.contains_key(entry_path) {
-                    return Err(ClientError::Function(vec![Error {
-                        code: 401,
-                        reason: "Error retrieve metadata".to_string(),
-                        message: "Can not found metadata for path, path maybe not a leaf entry"
-                            .to_string(),
-                    }]));
-                }
-
-                match str_to_value(value, datatype[entry_path]) {
-                    Ok(entry_value) => {
-                        let entry = EntryUpdate {
-                            fields: vec![Field::Value as i32],
-                            entry: Some(DataEntry {
-                                path: entry_path.to_string(),
-                                value: Some(Datapoint {
-                                    timestamp: Some(std::time::SystemTime::now().into()),
-                                    value: Some(entry_value),
-                                }),
-                                metadata: None,
-                                actuator_target: None,
-                            }),
-                        };
-
-                        if let Err(error) = self.set(vec![entry]).await {
-                            return Err(error);
-                        }
-
-                        Ok(())
-                    }
-
-                    Err(_) => {
-                        return Err(ClientError::Function(vec![Error {
-                            code: 400,
-                            reason: "Convert data value error".to_string(),
-                            message: "Can not convert string to {$datatype}".to_string(),
-                        }]));
-                    }
-                }
-            }
-
+        let datatype = match self.get_datatype(entry_path).await {
+            Ok(datatype) => datatype,
             Err(err) => {
                 // return METADATA error
                 return Err(err);
             }
+        };
+
+        if !datatype.contains_key(entry_path) {
+            return Err(ClientError::Function(vec![Error {
+                code: 401,
+                reason: "Error retrieve metadata".to_string(),
+                message: "Can not found metadata for path, path maybe not a leaf entry".to_string(),
+            }]));
         }
+
+        let entry_value = match str_to_value(value, datatype[entry_path]) {
+            Ok(entry_value) => entry_value,
+            Err(_) => {
+                return Err(ClientError::Function(vec![Error {
+                    code: 400,
+                    reason: "Convert data value error".to_string(),
+                    message: "Can not convert string to {$datatype}".to_string(),
+                }]));
+            }
+        };
+
+        let entry = EntryUpdate {
+            fields: vec![Field::Value as i32],
+            entry: Some(DataEntry {
+                path: entry_path.to_string(),
+                value: Some(Datapoint {
+                    timestamp: Some(std::time::SystemTime::now().into()),
+                    value: Some(entry_value),
+                }),
+                metadata: None,
+                actuator_target: None,
+            }),
+        };
+
+        self.set(vec![entry]).await
     }
 
     pub async fn subscribe_entry(
@@ -330,31 +305,31 @@ impl KuksaClient {
         println!("------ subcribe leaf entry:");
         println!("entry_path: {}", entry_path);
 
-        match self.client {
+        let client = match self.client {
             None => {
                 // TODO: connect to server
-                Err(ClientError::Connection(
+                return Err(ClientError::Connection(
                     "Please connect to server".to_string(),
-                ))
+                ));
             }
-            Some(ref mut client) => {
-                let entries = vec![SubscribeEntry {
-                    path: entry_path.to_string(),
-                    view: View::CurrentValue.into(),
-                    fields: vec![Field::Value.into()],
-                }];
+            Some(ref mut client) => client,
+        };
 
-                let request = SubscribeRequest { entries };
+        let entries = vec![SubscribeEntry {
+            path: entry_path.to_string(),
+            view: View::CurrentValue.into(),
+            fields: vec![Field::Value.into()],
+        }];
 
-                // call subcribes method
-                match client.subscribe(request).await {
-                    Ok(response) => {
-                        return Ok(response.into_inner());
-                    }
-                    Err(err) => {
-                        return Err(ClientError::Status(err));
-                    }
-                }
+        let request = SubscribeRequest { entries };
+
+        // call subcribes method
+        match client.subscribe(request).await {
+            Ok(response) => {
+                return Ok(response.into_inner());
+            }
+            Err(err) => {
+                return Err(ClientError::Status(err));
             }
         }
     }
